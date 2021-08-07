c4da4caf9c9afbd72189541377ca610c97caf26a
guopeng li <guopeng.li@technicolor.com>
2021-08-07 09:49:30 +0200

    add animation to splash screen + remove tool bar

--- app/src/main/java/com/lig/favdish/SplashActivity.kt
+++ app/src/main/java/com/lig/favdish/SplashActivity.kt
@@ -1,8 +1,16 @@
 package com.lig.favdish
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.lig.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -13,6 +21,31 @@ class SplashActivity : AppCompatActivity() {
         val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        }
+
+
+        val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        splashBinding.tvAppName.animation = splashAnimation
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+                    finish()
+                }, 1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+        })
     }
 
 
