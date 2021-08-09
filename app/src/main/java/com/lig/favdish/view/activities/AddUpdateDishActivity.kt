89384e1435e3fc0368523830c4eea44714852579
guopeng li <guopeng.li@technicolor.com>
2021-08-09 17:24:37 +0200

    Glide to display image

--- app/src/main/java/com/lig/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/lig/favdish/view/activities/AddUpdateDishActivity.kt
@@ -4,12 +4,16 @@ import android.Manifest
 import android.app.Activity
 import android.app.Dialog
 import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.Settings
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
@@ -17,6 +21,13 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -28,10 +39,16 @@ import com.karumi.dexter.listener.single.PermissionListener
 import com.lig.favdish.R
 import com.lig.favdish.databinding.ActivityAddUpdateDishBinding
 import com.lig.favdish.databinding.DialogCustomImageSelectionBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mBinding: ActivityAddUpdateDishBinding
+    private var mImagePath: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,9 +85,14 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         registerForActivityResult(ActivityResultContracts.StartActivityForResult())
         { result: ActivityResult ->
             if (result.resultCode == Activity.RESULT_OK) {
-                result?.data?.let {
+                result?.data?.extras?.let {
                     val thumbnail: Bitmap = result.data!!.extras!!.get("data") as Bitmap
-                    mBinding.ivDishImage.setImageBitmap(thumbnail)
+                    Glide.with(this)
+                        .load(thumbnail)
+                        .centerCrop()
+                        .into(mBinding.ivDishImage)
+                    mImagePath = saveImageToInternalStorage(thumbnail)
+                    Log.i("ImagePath", mImagePath)
                     mBinding.ivAddDishImage.setImageDrawable(
                         ContextCompat.getDrawable(
                             this,
@@ -79,8 +101,65 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
                     )
                 }
             }
-
         }
+
+    val startForGalleryResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+
+                result?.data?.let {
+                    val selectedPhototUri = result.data!!.data
+                    Log.i("ImagePath0:", selectedPhototUri.toString())
+
+                    Glide.with(this)
+                        .load(selectedPhototUri)
+                        .centerCrop()
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .listener(object : RequestListener<Drawable>{
+                            override fun onLoadFailed(
+                                e: GlideException?,
+                                model: Any?,
+                                target: Target<Drawable>?,
+                                isFirstResource: Boolean
+                            ): Boolean {
+                                return false
+                            }
+
+                            override fun onResourceReady(
+                                resource: Drawable?,
+                                model: Any?,
+                                target: Target<Drawable>?,
+                                dataSource: DataSource?,
+                                isFirstResource: Boolean
+                            ): Boolean {
+                                resource?.let {
+                                    val bitmap: Bitmap = resource.toBitmap()
+                                    mImagePath = saveImageToInternalStorage(bitmap)
+                                    Log.i("ImagePath", mImagePath)
+
+                                }
+                                //true if the listener has handled setting the resource on the target,
+                                // false to allow Glide's request to update the target.
+                                // Setting the resource includes handling animations, be sure to take that into account.
+                                return false
+                            }
+
+                        })
+                        .into(mBinding.ivDishImage)
+
+                    mBinding.ivAddDishImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            this,
+                            R.drawable.ic_edit
+                        )
+                    )
+                }
+            } else if (result.resultCode == Activity.RESULT_CANCELED) {
+                Log.e("Cancelled", "User cancl selection")
+            }
+        }
+
 
     private fun customImageSelectionDialog() {
         val dialog = Dialog(this)
@@ -98,7 +177,6 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
                         if (report.areAllPermissionsGranted()) {
                             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
                             startForCameraResult.launch(intent)
-
                         }
                     }
                 }
@@ -121,11 +199,9 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
                 Manifest.permission.READ_EXTERNAL_STORAGE,
             ).withListener(object : PermissionListener {
                 override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
-                    Toast.makeText(
-                        this@AddUpdateDishActivity,
-                        "You have gallery permission now",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    val galleryIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    startForGalleryResult.launch(galleryIntent)
                 }
 
                 override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
@@ -172,9 +248,26 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
             }.show()
     }
 
+    private fun saveImageToInternalStorage(bitmap: Bitmap): String {
+        val wrapper = ContextWrapper(applicationContext)
+        var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+        file = File(file, "${UUID.randomUUID()}.jpg")
+
+        try {
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return file.absolutePath
+
+    }
 
     companion object {
-
+        private const val IMAGE_DIRECTORY = "FavDishImage"
     }
 
 }
