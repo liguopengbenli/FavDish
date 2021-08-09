d8930f860a429176943c86df0fd053cd33c78002
guopeng li <guopeng.li@technicolor.com>
2021-08-09 11:52:14 +0200

    set tool bar and simple dialog

--- app/src/main/java/com/lig/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/lig/favdish/view/activities/AddUpdateDishActivity.kt
@@ -1,12 +1,62 @@
 package com.lig.favdish.view.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.lig.favdish.R
+import com.lig.favdish.databinding.ActivityAddUpdateDishBinding
+import com.lig.favdish.databinding.DialogCustomImageSelectionBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        setActionBar()
+        mBinding.ivAddDishImage.setOnClickListener(this)
+
     }
+
+
+    private fun setActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.iv_add_dish_image -> {
+                    customImageSelectionDialog()
+                    return
+                }
+            }
+        }
+    }
+
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this)
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+        binding.tvCamera.setOnClickListener {
+            dialog.dismiss()
+        }
+        binding.tvGallery.setOnClickListener {
+            dialog.dismiss()
+
+        }
+        dialog.show()
+    }
+
+
 }
