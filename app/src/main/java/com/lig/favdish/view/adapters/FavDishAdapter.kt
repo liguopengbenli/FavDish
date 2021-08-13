1de3acb458cc8ae858795da6de7fc70730a915de
guopeng li <guopeng.li@technicolor.com>
2021-08-13 18:16:21 +0200

    add pop up menu in each item

--- app/src/main/java/com/lig/favdish/view/adapters/FavDishAdapter.kt
+++ app/src/main/java/com/lig/favdish/view/adapters/FavDishAdapter.kt
@@ -1,13 +1,18 @@
 package com.lig.favdish.view.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.lig.favdish.R
 import com.lig.favdish.databinding.ItemDishBinding
 import com.lig.favdish.model.entities.FavDish
 import com.lig.favdish.view.fragments.AllDishesFragment
+import com.lig.favdish.view.fragments.FavoriteDishesFragment
 
 class FavDishAdapter(private val fragment: Fragment) :
     RecyclerView.Adapter<FavDishAdapter.ViewHolder>() {
@@ -17,11 +22,13 @@ class FavDishAdapter(private val fragment: Fragment) :
     class ViewHolder(view: ItemDishBinding) : RecyclerView.ViewHolder(view.root) {
         val ivDishImage = view.ivDishImage
         val tvTitle = view.tvDishTitle
+        val ibMore = view.ibMore
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding: ItemDishBinding = ItemDishBinding.inflate(
-            LayoutInflater.from(fragment.context), parent, false)
+            LayoutInflater.from(fragment.context), parent, false
+        )
         return ViewHolder(binding)
     }
 
@@ -37,7 +44,34 @@ class FavDishAdapter(private val fragment: Fragment) :
             if (fragment is AllDishesFragment) {
                 fragment.dishDetails(dish)
             }
+            if (fragment is FavoriteDishesFragment) {
+                fragment.dishDetails(dish)
+            }
         }
+
+        holder.ibMore.setOnClickListener {
+            val popup = PopupMenu(fragment.context, holder.ibMore)
+            popup.menuInflater.inflate(R.menu.menu_adapter, popup.menu)
+
+            popup.setOnMenuItemClickListener {
+                if (it.itemId == R.id.action_edit_dish) {
+                    Toast.makeText(fragment.context, "edit dish", Toast.LENGTH_SHORT).show()
+                } else if (it.itemId == R.id.action_delete_dish) {
+                    Toast.makeText(fragment.context, "delete dish", Toast.LENGTH_SHORT).show()
+
+                }
+                true
+            }
+
+            popup.show()
+        }
+
+        if (fragment is AllDishesFragment) {
+            holder.ibMore.visibility = View.VISIBLE
+        }else if (fragment is FavoriteDishesFragment) {
+            holder.ibMore.visibility = View.GONE
+        }
+
     }
 
     override fun getItemCount(): Int = dishes.size
--- app/src/main/java/com/lig/favdish/view/fragments/FavoriteDishesFragment.kt
+++ app/src/main/java/com/lig/favdish/view/fragments/FavoriteDishesFragment.kt
@@ -11,9 +11,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.lig.favdish.application.FavDishApplication
 import com.lig.favdish.databinding.FragmentFavoriteDishesBinding
+import com.lig.favdish.model.entities.FavDish
+import com.lig.favdish.view.activities.MainActivity
 import com.lig.favdish.view.adapters.FavDishAdapter
 import com.lig.favdish.viewmodel.DashboardViewModel
 import com.lig.favdish.viewmodel.FavDishViewModel
@@ -66,8 +69,25 @@ class FavoriteDishesFragment : Fragment() {
         }
     }
 
+    fun dishDetails(favDish: FavDish) {
+        findNavController().navigate(FavoriteDishesFragmentDirections
+            .actionNavigationFavoriteDishesToDishDetailFragment(favDish))
+        if (requireActivity() is MainActivity) {
+            (activity as MainActivity).hideBottomNavigationView()
+        }
+
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onResume() {
+        super.onResume()
+        if (requireActivity() is MainActivity) {
+            (activity as MainActivity).showBottomNavigationView()
+        }
+    }
+
 }
