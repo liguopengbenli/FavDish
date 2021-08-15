161a84ec105077ac502467b6c5c6c2cfb10c097e
guopeng li <guopeng.li@technicolor.com>
2021-08-15 17:15:08 +0200

    add dish to favorite + refresh random dish feature

--- app/src/main/java/com/lig/favdish/view/activities/MainActivity.kt
+++ app/src/main/java/com/lig/favdish/view/activities/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.lig.favdish.view.activities
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -27,7 +27,9 @@ class MainActivity : AppCompatActivity() {
         mNavController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_all_dishes, R.id.navigation_favorite_dishes, R.id.navigation_random_dish
+                R.id.navigation_all_dishes,
+                R.id.navigation_favorite_dishes,
+                R.id.navigation_random_dish
             )
         )
         setupActionBarWithNavController(mNavController, appBarConfiguration)
@@ -44,13 +46,15 @@ class MainActivity : AppCompatActivity() {
     fun hideBottomNavigationView() {
         binding.navView.clearAnimation()
         binding.navView.animate().translationY(binding.navView.height.toFloat()).duration = 300
+        binding.navView.visibility = View.GONE
     }
 
     fun showBottomNavigationView() {
         binding.navView.clearAnimation()
         binding.navView.animate().translationY(0f).duration = 300
-    }
+        binding.navView.visibility = View.VISIBLE
 
+    }
 
 
 }
--- app/src/main/java/com/lig/favdish/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/com/lig/favdish/view/fragments/RandomDishFragment.kt
@@ -1,13 +1,26 @@
 package com.lig.favdish.view.fragments
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.lig.favdish.R
+import com.lig.favdish.application.FavDishApplication
 import com.lig.favdish.databinding.FragmentRandomDishBinding
+import com.lig.favdish.model.entities.FavDish
+import com.lig.favdish.model.entities.RandomDish
+import com.lig.favdish.util.Constants
+import com.lig.favdish.viewmodel.FavDishViewModel
+import com.lig.favdish.viewmodel.FavDishViewModelFactory
 import com.lig.favdish.viewmodel.NotificationsViewModel
 import com.lig.favdish.viewmodel.RandomDishViewModel
 
@@ -42,6 +55,10 @@ class RandomDishFragment : Fragment() {
         randomDishViewModel.getRandomRecipeFromApi()
 
         randomDishViewModelObserver()
+
+        binding.srlRandomDish.setOnRefreshListener {
+            randomDishViewModel.getRandomRecipeFromApi()
+        }
     }
 
     private fun randomDishViewModelObserver() {
@@ -49,13 +66,19 @@ class RandomDishFragment : Fragment() {
             { randomDishResponse ->
                 randomDishResponse?.let {
                     Log.d("randomDish", "lig !!! $it")
+                    if (binding.srlRandomDish.isRefreshing) {
+                        binding.srlRandomDish.isRefreshing = false
+                    }
+                    setRandomDishResponseInUI(randomDishResponse.recipes[0])
                 }
             }
         )
         randomDishViewModel.randomDishLoadingError.observe(viewLifecycleOwner,
             { error ->
                 error?.let {
-
+                    if (binding.srlRandomDish.isRefreshing) {
+                        binding.srlRandomDish.isRefreshing = false
+                    }
                 }
             }
         )
@@ -65,6 +88,91 @@ class RandomDishFragment : Fragment() {
             }
         })
 
+    }
+
+    private fun setRandomDishResponseInUI(recipe: RandomDish.Recipe) {
+        Glide.with(requireActivity())
+            .load(recipe.image)
+            .centerCrop()
+            .into(binding.ivDishImage)
+
+        binding.apply {
+            tvTitle.text = recipe.title
+            var dishType: String = "other"
+            if (recipe.dishTypes.isNotEmpty()) {
+                dishType = recipe.dishTypes[0]
+                tvType.text = dishType
+            }
+            tvCategory.text = "Other"
+            var ingredients = ""
+            for (value in recipe.extendedIngredients) {
+                if (ingredients.isEmpty()) {
+                    ingredients = value.original
+                } else {
+                    ingredients = ingredients + ", \n" + value.original
+                }
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                // read html format
+                tvCookingDirection.text = Html.fromHtml(
+                    recipe.instructions,
+                    Html.FROM_HTML_MODE_COMPACT
+                )
+            } else {
+                @Suppress("DEPRECATION")
+                tvCookingDirection.text = Html.fromHtml(recipe.instructions)
+            }
+
+            binding.ivFavoriteDish.setImageDrawable(
+                ContextCompat.getDrawable(
+                    requireActivity(),
+                    R.drawable.ic_favorite_unselected
+                )
+            )
+
+            var addedToFavorites = false
+
+            tvCookingTime.text = resources.getString(
+                R.string.lbl_cooking_time_in_minutes,
+                recipe.readyInMinutes.toString()
+            )
+            val mFavDishViewModel: FavDishViewModel by viewModels {
+                FavDishViewModelFactory((requireActivity().application as FavDishApplication).repository)
+            }
+
+            ivFavoriteDish.setOnClickListener {
+                if (addedToFavorites) {
+                    Toast.makeText(
+                        requireActivity(),
+                        resources.getString(R.string.msg_already_added_to_favorites),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    val randomDishDetails = FavDish(
+                        recipe.image,
+                        Constants.DISH_IMAGE_SOURCE_ONLINE,
+                        recipe.title,
+                        dishType,
+                        "other",
+                        ingredients,
+                        recipe.readyInMinutes.toString(),
+                        recipe.instructions,
+                        true
+                    )
+                    mFavDishViewModel.insert(randomDishDetails)
+
+                    addedToFavorites = true
+
+                    ivFavoriteDish.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            requireActivity(),
+                            R.drawable.ic_favorite_selected
+                        )
+                    )
+                }
+            }
+
+        }
     }
 
     override fun onDestroyView() {
